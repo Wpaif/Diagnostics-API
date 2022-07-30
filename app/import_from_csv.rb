8e8a76ddb45b974fb04498e414e83d88f65801bd
Wilian Ferreira <wpaife@gmail.com>
2022-07-30 18:39:48 -0300

    Ajusta insersão de dados de forma assíncrona

--- app/import_from_csv.rb
+++ app/import_from_csv.rb
@@ -1,4 +1,4 @@
-require_relative './sidekiq/import_worker'
+require_relative 'sidekiq/import_worker'
 
 CSV.foreach("#{Dir.pwd}/data.csv", headers: true, col_sep: ';') do |row|
   ImportWorker.perform_async(row.fields, 'db')
