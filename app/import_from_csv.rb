4e83f3f29e629e25c3f8c8f77051f2019b2c51fd
Wilian Ferreira <wpaife@gmail.com>
2022-07-30 19:45:02 -0300

    Ajusta formatação de arquivos

--- app/import_from_csv.rb
+++ app/import_from_csv.rb
@@ -1,5 +1,5 @@
 require_relative 'sidekiq/import_worker'
 
-CSV.foreach("#{Dir.pwd}/data.csv", headers: true, col_sep: ';') do |row|
-  ImportWorker.perform_async(row.fields, 'db')
+CSV.foreach("#{Dir.pwd}/data.csv", headers: true, col_sep: ';') do |register|
+  ImportWorker.perform_async(register.fields, 'db')
 end
